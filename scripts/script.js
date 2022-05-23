cffdd2a6b854f754932ed26643e2a48f46af9128
MintFuze <vadim801vadim@gmail.com>
2022-05-24 00:00:19 +0300

    update cv

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,27 @@
-document.getElementById("main-action").onclick = function () {
-    document.getElementById("about-me").scrollIntoView({behavior: "smooth"});
-}
 
-// var buttons = document.getElementsByClassName("car-button");
-//
-// for (var i = 0; i<buttons.length; i++){
-//     buttons[i].onclick = function () {
-//         document.getElementById("price").scrollIntoView({behavior: "smooth"});
-//     }
-// }
-//
-// document.getElementById("price-action").onclick = function () {
-// if (document.getElementById("name").value === "") {
-//     alert("Заполните поле имя!");
-// } else if (document.getElementById("phone").value === "") {
-//     alert("Заполните поле телефона!");
-// } else if (document.getElementById("car").value === "") {
-//     alert("Заполните поле автомобиля!");
-// }else {
-// alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!")}
-// }
 
-document.addEventListener("DOMContentLoaded", function () {
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
 
-    const elem = document.querySelector(".home");
-    document.addEventListener('scroll', () => {
-        elem.style.backgroundPositionX = '0' + (0.7 * window.pageYOffset) + 'px';
-    })
-});
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     const elem = document.querySelector(".home");
+//     document.addEventListener('scroll', () => {
+//         elem.style.backgroundPositionY = '0' + (0.1 * window.pageXOffset) + 'px';
+//     })
+// });
 
